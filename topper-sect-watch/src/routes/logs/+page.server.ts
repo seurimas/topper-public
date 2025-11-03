c3db865cd09479d638a1db5d8164a8844fee0b97
Nicholas <nicholasanderson5@gmail.com>
2025-11-02 22:31:56 -0600

    Add sect logs list.

--- topper-sect-watch/src/routes/logs/+page.server.ts
+++ topper-sect-watch/src/routes/logs/+page.server.ts
@@ -2,6 +2,10 @@ import { error } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 import { STORAGE_BUCKET_NAME } from '$lib/sect_logs';
 
+const I_WIN_REGEX = /^\((\w+)\)_(\w+)_vs_(\w+)_(\w+)_(\d+)$/;
+const YOU_WIN_REGEX = /^(\w+)_(\w+)_vs_\((\w+)\)_(\w+)_(\d+)$/;
+const DRAW_REGEX = /^(\w+)_(\w+)_vs_(\w+)_(\w+)_(\d+)$/;
+
 export const load: PageServerLoad = async ({ locals: { supabase } }) => {
 
     const { data, error: storage_error } = await supabase.storage
@@ -12,5 +16,44 @@ export const load: PageServerLoad = async ({ locals: { supabase } }) => {
         error(404, 'Log not found');
     }
 
-    return { logs: data.map(item => item.name.replace('.json', '')) };
+    const names = data.map(item => item.name.replace('.json', ''));
+    const logs = names.map(name => {
+        if (I_WIN_REGEX.test(name)) {
+            const [, myName, myClass, oppName, oppClass, length] = I_WIN_REGEX.exec(name)!;
+            return {
+                name,
+                myName,
+                myClass,
+                oppName,
+                oppClass,
+                length: parseInt(length),
+                winner: myName,
+            };
+        } else if (YOU_WIN_REGEX.test(name)) {
+            const [, myName, myClass, oppName, oppClass, length] = YOU_WIN_REGEX.exec(name)!;
+            return {
+                name,
+                myName,
+                myClass,
+                oppName,
+                oppClass,
+                length: parseInt(length),
+                winner: oppName,
+            };
+        } else if (DRAW_REGEX.test(name)) {
+            const [, myName, myClass, oppName, oppClass, length1,] = DRAW_REGEX.exec(name)!;
+            return {
+                name,
+                myName,
+                myClass,
+                oppName,
+                oppClass,
+                length: parseInt(length1), // both lengths are the same in a draw
+                winner: 'draw',
+            };
+        }
+        return { name, myName: 'unknown', myClass: 'unknown', oppName: 'unknown', oppClass: 'unknown', length: 0, winner: 'unknown' };
+    });
+
+    return { logs };
 }
--- topper-sect-watch/src/routes/logs/+page.svelte
+++ topper-sect-watch/src/routes/logs/+page.svelte
@@ -1,8 +1,104 @@
 <script lang="ts">
+    import { createGrid, ModuleRegistry, AllCommunityModule } from 'ag-grid-community';
+	import { onMount } from 'svelte';
+
+    ModuleRegistry.registerModules([AllCommunityModule]);
+
+    const columnDefs = [
+        {
+            field: 'name',
+            headerName: 'Link',
+            cellRenderer: (params: { value: string }) => {
+                return `<a class="text-indigo-400 hover:underline" href="/logs/${params.value}">${params.value}</a>`;
+            }
+        },
+        {
+            field: 'myName',
+            headerName: 'Shared By',
+            sortable: true,
+            filter: true,
+        },
+        {
+            field: 'myClass',
+            headerName: 'Class',
+            sortable: true,
+            filter: true,
+        },
+        {
+            field: 'oppName',
+            headerName: 'Opponent',
+            sortable: true,
+            filter: true,
+        },
+        {
+            field: 'oppClass',
+            headerName: 'Class',
+            sortable: true,
+            filter: true,
+        },
+        {
+            field: 'winner',
+            headerName: 'Winner',
+            sortable: true,
+            filter: true,
+        }
+    ];
+
+    import { themeQuartz } from 'ag-grid-community';
+
+    const myTheme = themeQuartz
+        .withParams({
+            accentColor: "#15BDE8",
+            backgroundColor: "#0C0C0D",
+            borderColor: "#ffffff00",
+            borderRadius: 20,
+            browserColorScheme: "dark",
+            cellHorizontalPaddingScale: 1,
+            chromeBackgroundColor: {
+                ref: "backgroundColor"
+            },
+            columnBorder: false,
+            fontFamily: {
+                googleFont: "Roboto"
+            },
+            fontSize: 16,
+            foregroundColor: "#BBBEC9",
+            headerBackgroundColor: "#182226",
+            headerFontSize: 14,
+            headerFontWeight: 500,
+            headerTextColor: "#FFFFFF",
+            headerVerticalPaddingScale: 0.9,
+            iconSize: 20,
+            rowBorder: false,
+            rowVerticalPaddingScale: 1.2,
+            sidePanelBorder: false,
+            spacing: 8,
+            wrapperBorder: false,
+            wrapperBorderRadius: 0
+        });
+
+
+    let gridElement: HTMLDivElement;
+
     let { data } = $props();
     let { logs } = $derived(data);
+
+    const gridOptions = $derived({
+        rowData: logs,
+        columnDefs,
+        theme: myTheme,
+    });
+
+    onMount(() => {
+        createGrid(gridElement, gridOptions);
+    });
 </script>
 
-<div>{logs.length} logs found</div>
+<div class="mx-auto my-8 max-w-7xl px-4 sm:px-6 lg:px-8">
+    <h1 class="text-3xl font-bold mb-4">Public Sect Logs</h1>
+    <div class="ag-theme-alpine" style="height: 600px; width: 100%;" bind:this={gridElement}>
+        
+    </div>
+</div>
 
 <style></style>
