c1ab5085ad56cc273f4bf8d9872d2919140e0d7d
Nicholas <nicholasanderson5@gmail.com>
2025-11-08 18:37:09 -0600

    Do not require API key for viewing API info.

--- topper-sect-watch/src/routes/api/+page.server.ts
+++ topper-sect-watch/src/routes/api/+page.server.ts
@@ -5,8 +5,5 @@ import { STORAGE_BUCKET_NAME, type ExplainerPage } from '$lib/sect_logs';
 export const load: PageServerLoad = async ({ locals: { supabase } }) => {
     const { data: api_key } = await supabase.functions.invoke('get-key');
 
-    if (!api_key) {
-        throw error(500, 'Failed to retrieve API key');
-    }
     return { apiKey: api_key };
 }
